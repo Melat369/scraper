da875ed10c5c1b3a5f2f8cb7de690ce1c22607c1
Melat369 <melattesfaye16@gmail.com>
2023-09-05 20:44:36 +0300

    importing libraries for regression model

--- project1.py
+++ project1.py
@@ -1,10 +1,5 @@
 print("****************************************Welcome to Melon Contact Manager****************************************")
 choice = input("Do you want to login[If You Have an Account] or Do you want to SignUp? login || signup : ")
-users = {
-    'user1': 'password1',
-    'user2': 'password2',
-    'user3': 'password3'
-}
 
 if(choice=="login"):
     
@@ -33,14 +28,23 @@ if(choice=="login"):
           print("Thank You!!! Good Bye!")   
 
 else:
+ users = {
+    'user1': 'password1',
+    'user2': 'password2',
+    'user3': 'password3'
+}
+
  print("****************************************Welcome our New user, kindly insert the required fields****************************************")
- UserName = input('Enter your user Name: ')
- Password = input('Enter your password: ')
+ userName = input('Enter your user Name: ')
+ password = input('Enter your password: ')
  reEnter = input('ReEnter your password: ')
- if(Password!=reEnter):
-    print('Re-Enter your password correctly')
- else:
+ if password == reEnter:
+    users[userName] = password
+    print(f'The username {userName} and password have been added to the dictionary for future use')
     print("Welcome")
+
+ else:
+    print('The entered passwords do not match')
 #the function that executes the creation deletion and insertion of contacts would be called  
     
 
